b7a62e2fb42fad966fda8d4cc0ea2fe99b670347
go and Mark Drastrup Olesen <g.nakano555+mark.drastrup@gmail.com>
2019-07-26 17:06:06 +0900

    Almost finished basics

--- models/userMessages/create.js
+++ models/userMessages/create.js
@@ -1,5 +1,28 @@
 module.exports = (knex, UserMessage) => {
-  return (params) => {
-    return Promise.resolve({}); // fix me!
+  return async (params) => {
+    if (params === undefined) {
+      return Promise.reject(
+        new Error(
+          "Channel message must be provided, and be at least two characters"
+        )
+      );
+    }
+    return knex("user_messages")
+      .insert({
+        from_id: params.fromId,
+        to_id: params.toId,
+        message: params.message,
+      })
+      .then(() => {
+        return knex
+          .from("user_messages")
+          .join("users", "user_messages.from_id", "users.id")
+          .select();
+      })
+      .then((message) => {
+        message[0].from = message[0].username;
+        const newMessage = new UserMessage(message.pop());
+        return [newMessage];
+      });
   };
 };
--- models/userMessages/list.js
+++ models/userMessages/list.js
@@ -1,5 +1,19 @@
 module.exports = (knex, UserMessage) => {
-  return (params) => {
-    return Promise.resolve([]); // fix me!
+  return async (params) => {
+    return knex("user_messages")
+      .whereIn("from_id", [params.fromId, params.toId])
+      .join("users", "user_messages.from_id", "users.id")
+      .then((messages) => {
+        console.log(messages);
+        const edited = messages.map((allMessage) => {
+          allMessage.from = allMessage.username;
+          return allMessage;
+        });
+        const newMessages = edited.map((message) => new UserMessage(message));
+        console.log("NEWMESSAGES", newMessages);
+        return newMessages;
+      });
+
+    //return Promise.resolve(newMessages); // fix me!
   };
 };
